488fea97d45f7c5ba97a8777e9e5be03769c6bde
Roilann <86485854+Roilann@users.noreply.github.com>
2024-01-12 14:26:00 +0100

    final tfrecord_generator update

--- Colab/Script/tfrecord_generator.py
+++ Colab/Script/tfrecord_generator.py
@@ -23,58 +23,56 @@ from object_detection.utils import dataset_util
 from collections import namedtuple
 
 
-def split(df, group):
-    data = namedtuple('data', ['filename', 'object'])
-    gb = df.groupby(group)
-    return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
-
-
-def create_tf_example(group, path):
-    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
-        encoded_jpg = fid.read()
-    encoded_jpg_io = io.BytesIO(encoded_jpg)
-    image = Image.open(encoded_jpg_io)
-    width, height = image.size
-
-    filename = group.filename.encode('utf8')
-    image_format = b'jpg'
-    xmins = []
-    xmaxs = []
-    ymins = []
-    ymaxs = []
-    classes_text = []
-    classes = []
-
-    labels = []
-    with open(labelmap_path, 'r') as f:
-        labels = [line.strip() for line in f.readlines()]
-
-    for index, row in group.object.iterrows():
-        xmins.append(row['xmin'] / width)
-        xmaxs.append(row['xmax'] / width)
-        ymins.append(row['ymin'] / height)
-        ymaxs.append(row['ymax'] / height)
-        classes_text.append(row['class'].encode('utf8'))
-        classes.append(int(labels.index(row['class']) + 1))
-
-    tf_example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': dataset_util.int64_feature(height),
-        'image/width': dataset_util.int64_feature(width),
-        'image/filename': dataset_util.bytes_feature(filename),
-        'image/source_id': dataset_util.bytes_feature(filename),
-        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
-        'image/format': dataset_util.bytes_feature(image_format),
-        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
-        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
-        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
-        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
-        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-        'image/object/class/label': dataset_util.int64_list_feature(classes),
-    }))
-    return tf_example
-
-
 def generate_tfrecord(csv_input_path, labelmap_path, image_dir_path, output_tfrecord_path):
+    def split(df, group):
+        data = namedtuple('data', ['filename', 'object'])
+        gb = df.groupby(group)
+        return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
+
+    def create_tf_example(group, path):
+        with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+            encoded_jpg = fid.read()
+        encoded_jpg_io = io.BytesIO(encoded_jpg)
+        image = Image.open(encoded_jpg_io)
+        width, height = image.size
+
+        filename = group.filename.encode('utf8')
+        image_format = b'jpg'
+        xmins = []
+        xmaxs = []
+        ymins = []
+        ymaxs = []
+        classes_text = []
+        classes = []
+
+        labels = []
+        with open(labelmap_path, 'r') as f:
+            labels = [line.strip() for line in f.readlines()]
+
+        for index, row in group.object.iterrows():
+            xmins.append(row['xmin'] / width)
+            xmaxs.append(row['xmax'] / width)
+            ymins.append(row['ymin'] / height)
+            ymaxs.append(row['ymax'] / height)
+            classes_text.append(row['class'].encode('utf8'))
+            classes.append(int(labels.index(row['class']) + 1))
+
+        tf_example = tf.train.Example(features=tf.train.Features(feature={
+            'image/height': dataset_util.int64_feature(height),
+            'image/width': dataset_util.int64_feature(width),
+            'image/filename': dataset_util.bytes_feature(filename),
+            'image/source_id': dataset_util.bytes_feature(filename),
+            'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+            'image/format': dataset_util.bytes_feature(image_format),
+            'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+            'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+            'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+            'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+            'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+            'image/object/class/label': dataset_util.int64_list_feature(classes),
+        }))
+        return tf_example
+
     # Load and prepare data
     writer = tf.python_io.TFRecordWriter(output_tfrecord_path)
     path = os.path.join(os.getcwd(), image_dir_path)
@@ -102,15 +100,20 @@ def generate_tfrecord(csv_input_path, labelmap_path, image_dir_path, output_tfre
 
 
 if __name__ == '__main__':
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+
+    train_csv_path = os.path.join(script_directory, 'images/train_labels.csv')
+    valid_csv_path = os.path.join(script_directory, 'images/valid_labels.csv')
+
+    label_path = os.path.join(script_directory, 'labelmap.txt')
+
+    images_train_path = os.path.join(script_directory, 'images/train')
+    images_valid_path = os.path.join(script_directory, 'images/valid')
+
+    output_train_path = os.path.join(script_directory, 'train.tfrecord')
+    output_valid_path = os.path.join(script_directory, 'valid.tfrecord')
+
     # train.tfrecord generation
-    generate_tfrecord(csv_input_path='/content/images/train_labels.csv',
-        labelmap_path='labelmap.txt',
-        image_dir_path='/content/images/train',
-        output_tfrecord_path='/content/train.tfrecord'
-    )
-    # val.tfrecord generation
-    generate_tfrecord(csv_input_path='/content/images/valid_labels.csv',
-        labelmap_path='labelmap.txt',
-        image_dir_path='/content/images/valid',
-        output_tfrecord_path='/content/val.tfrecord'
-    )
+    generate_tfrecord(train_csv_path, label_path, images_train_path, output_train_path)
+    # train.tfrecord generation
+    generate_tfrecord(valid_csv_path, label_path, images_valid_path, output_valid_path)
