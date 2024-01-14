872c8545022be5a731b149ac748d31f01b54770e
Roilann <86485854+Roilann@users.noreply.github.com>
2024-01-14 12:38:35 +0100

    rename + add deletion + add tfrecord_to_png

--- Colab/Script/roboflow_tfr_adapt.py
+++ Colab/Script/roboflow_tfr_adapt.py
@@ -13,6 +13,52 @@ import shutil
 from zipfile import ZipFile
 import re
 import time
+import tensorflow as tf
+
+
+def _parse_function(proto):
+    # Define your feature names and types here
+    keys_to_features = {
+        'image/height': tf.io.FixedLenFeature([], tf.int64),
+        'image/width': tf.io.FixedLenFeature([], tf.int64),
+        'image/filename': tf.io.FixedLenFeature([], tf.string),
+        'image/source_id': tf.io.FixedLenFeature([], tf.string, default_value=''),  # Set a default value
+        'image/encoded': tf.io.FixedLenFeature([], tf.string),
+        'image/format': tf.io.FixedLenFeature([], tf.string),
+        'image/object/bbox/xmin': tf.io.VarLenFeature(tf.float32),
+        'image/object/bbox/xmax': tf.io.VarLenFeature(tf.float32),
+        'image/object/bbox/ymin': tf.io.VarLenFeature(tf.float32),
+        'image/object/bbox/ymax': tf.io.VarLenFeature(tf.float32),
+        'image/object/class/text': tf.io.VarLenFeature(tf.string),
+        'image/object/class/label': tf.io.VarLenFeature(tf.int64),
+    }
+
+    # Parse the input tf.Example proto using the dictionary above
+    parsed_features = tf.io.parse_single_example(proto, keys_to_features)
+
+    # Handle the case where 'image/source_id' is not present
+    if parsed_features['image/source_id'] == '':
+        parsed_features['image/source_id'] = tf.constant('', dtype=tf.string)
+
+    return parsed_features
+
+
+def load_tfrecord_dataset(tfrecord_path):
+    raw_dataset = tf.data.TFRecordDataset(tfrecord_path)
+    parsed_dataset = raw_dataset.map(_parse_function)
+    return parsed_dataset
+
+
+def extract_images(dataset, output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+
+    for i, example in enumerate(dataset):
+        image_encoded = example['image/encoded'].numpy()
+
+        # Save images in PNG format with simple convention
+        image_path = os.path.join(output_dir, f"test_output_{i}.png")
+        with open(image_path, 'wb') as f:
+            f.write(image_encoded)
 
 
 def parse_labelmap(labelmap_content):
@@ -65,6 +111,10 @@ start_time = time.time()
 
 # Step 1: Get the current script directory
 script_directory = os.path.dirname(os.path.abspath(__file__))
+# Folder that contains images
+images_path = os.path.join(script_directory, 'images')
+create_folder(images_path)
+# Folder to work with
 output_path = os.path.join(script_directory, 'tfrecord')
 create_folder(output_path)
 labelmap_txt_path = os.path.join(output_path, 'labelmap.txt')
@@ -137,16 +187,19 @@ step_4 = time.time()
 et_step_4 = step_4 - step_3
 print(f"\n\n--Time : Step 3 to Step 4: {et_step_4:.4f} seconds--\n\n")
 
+# Deletion
+for zip_file in zip_files:
+    os.remove(zip_file)
+
+# Step 5: Convert test tfrecord to png
+
+tfrecord_path = os.path.join(output_path, 'test'+'.tfrecord')
+output_pic_directory = os.path.join(images_path, 'test')
+dataset = load_tfrecord_dataset(tfrecord_path)
+extract_images(dataset, output_pic_directory)
+print(f"Images Successfully Extracted from tfrecord file")
+
 # Time
 end_time = time.time()
 elapsed_time = end_time - start_time
 print(f"\n\n--Time : Execution time: {elapsed_time:.4f} seconds--\n\n")
-
-# Deletion
-
-# Time
-end_time_deletion = time.time()
-elapsed_time_deletion = end_time_deletion - end_time
-print(f"\n\n--Time : Deletion time: {elapsed_time_deletion:.4f} seconds--\n\n")
-complete_elapsed_time = end_time_deletion - start_time
-print(f"\n\n--Time : Complete Execution time: {complete_elapsed_time:.4f} seconds--\n\n")
