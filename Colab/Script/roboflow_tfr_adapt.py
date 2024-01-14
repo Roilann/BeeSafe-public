f0cdc6f24cfb0919a66233f698f6024f86977379
Roilann <86485854+Roilann@users.noreply.github.com>
2024-01-14 02:21:06 +0100

    update to roboflow_tfr_adapt

--- Colab/Script/roboflow_tfr_adapt.py
+++ Colab/Script/roboflow_tfr_adapt.py
@@ -10,8 +10,9 @@
 
 import os
 import shutil
-import re
 from zipfile import ZipFile
+import re
+import time
 
 
 def parse_labelmap(labelmap_content):
@@ -26,75 +27,119 @@ def parse_labelmap(labelmap_content):
     return result
 
 
+def contains_subfolder(folder_path):
+    if os.path.isdir(folder_path):
+        subfolders = [item for item in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, item))]
+        if subfolders:
+            return True, subfolders
+        else:
+            return False, []
+    else:
+        raise ValueError(f"The provided path '{folder_path}' is not a valid directory.")
+
+
+def create_folder(folder_path):
+    # Check if the folder already exists
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+
 def unzip_file(zip_path, extract_path):
     with ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_path)
 
 
-def main():
-    # Step 1: Get the current script directory
-    script_directory = os.path.dirname(os.path.abspath(__file__))
+# Measure execution time
+start_time = time.time()
 
-    # Step 2: Identify the only zip file and extracted folder dynamically
-    zip_files = [file for file in os.listdir(script_directory) if file.endswith('.zip')]
-    if len(zip_files) != 1:
-        print("Error: There should be exactly one zip file in the current directory.")
-        return
+# Step 1: Get the current script directory and create needed directories
+script_directory = os.path.dirname(os.path.abspath(__file__))
+# Folder that contains images
+images_path = os.path.join(script_directory, 'images')
+create_folder(images_path)
+# Folder to work with
+output_path = os.path.join(script_directory, 'tfrecord')
+create_folder(output_path)
 
-    zip_file = zip_files[0]
+# Step 2: Identify the zip file(s) and extracted folder dynamically
+zip_files = [file for file in os.listdir(script_directory) if file.endswith('.zip')]
+nb_zip_files = len(zip_files)
+if nb_zip_files != 1 and nb_zip_files != 3:
+    raise ValueError(f"Error: There should be 1 zip file or 3 in the directory but there was {nb_zip_files}.")
+
+# Step 3: Unzip data and place it in unzip
+for zip_file in zip_files:
     extracted_folder = os.path.splitext(zip_file)[0]
 
-    # Step 3: Unzip the identified zip file into the extracted folder
+    # Step 3.1: Unzip the identified zip file into the extracted folder
     zip_path = os.path.join(script_directory, zip_file)
     extract_path = os.path.join(script_directory, extracted_folder)
+    print(f"\nStart {zip_file} extraction")
     unzip_file(zip_path, extract_path)
+    print(f"{zip_file} is extracted")
 
-    # Step 4: Identify subfolders within the extracted folder
-    subfolders = [subfolder for subfolder in os.listdir(extract_path) if
-                  os.path.isdir(os.path.join(extract_path, subfolder))]
+    # Step 3.2: Identify subfolders within the extracted folder
+    result, subfolders = contains_subfolder(os.path.join(script_directory, extracted_folder))
 
-    # Step 5: Create the output folder (tfrecord)
-    output_path = os.path.join(script_directory, 'tfrecord')
-    os.makedirs(output_path)
+    if result:
+        # Step 3.3: Process subfolders (train, test, valid)
+        print(f"Start copy of folders")
+        for folder_name in subfolders:
+            folder_path = os.path.join(extract_path, folder_name)
+            folder_images_path = os.path.join(images_path, folder_name)
 
-    # Step 6: Process subfolders (train, test, valid)
-    for folder_name in subfolders:
-        folder_path = os.path.join(extract_path, folder_name)
-        tf_file = [file for file in os.listdir(folder_path) if file.endswith('.tfrecord')]
+            shutil.move(folder_path, folder_images_path)
 
-        if len(tf_file) > 1:
-            raise ValueError(f"Error: More than one .tfrecord file found in folder {folder_name}")
+        shutil.rmtree(extracted_folder)
+    else:
+        folder_images_path = os.path.join(images_path, extracted_folder)
+        shutil.move(extracted_folder, folder_images_path)
 
-        if len(tf_file) == 0:
-            raise ValueError(f"Error: No .tfrecord file found in folder {folder_name}")
+# Step 5: Create the output folder (tfrecord)
+output_path = os.path.join(script_directory, 'tfrecord')
+os.makedirs(output_path)
 
-        tf_file_path = os.path.join(folder_path, tf_file[0])
-        tf_file_output_path = os.path.join(output_path, f"{folder_name}.tfrecord")
+# Step 6: Process subfolders (train, test, valid)
+for folder_name in subfolders:
+    folder_path = os.path.join(extract_path, folder_name)
+    tf_file = [file for file in os.listdir(folder_path) if file.endswith('.tfrecord')]
 
-        os.rename(tf_file_path, tf_file_output_path)
+    if len(tf_file) > 1:
+        raise ValueError(f"Error: More than one .tfrecord file found in folder {folder_name}")
 
-    # Step 7: Get the labelmap
-    first_subfolder_path = os.path.join(extract_path, subfolders[0])
-    labelmap_file = [file for file in os.listdir(folder_path) if file.endswith('.pbtxt')]
-    labelmap_path = os.path.join(first_subfolder_path, labelmap_file[0])
-    labelmap_txt_path = os.path.join(output_path, 'labelmap.txt')
-    labelmap_pbtxt_path = os.path.join(output_path, 'labelmap.pbtxt')
+    if len(tf_file) == 0:
+        raise ValueError(f"Error: No .tfrecord file found in folder {folder_name}")
 
-    os.rename(labelmap_path, labelmap_pbtxt_path)
+    tf_file_path = os.path.join(folder_path, tf_file[0])
+    tf_file_output_path = os.path.join(output_path, f"{folder_name}.tfrecord")
 
-    # Step 8: Take labelmap.pbtxt to create labelmap.txt tf standard
-    with open(labelmap_pbtxt_path, 'r') as file:
-        labelmap_content = file.read()
+    os.rename(tf_file_path, tf_file_output_path)
 
-    parsed_data = parse_labelmap(labelmap_content)
+# Step 7: Get the labelmap
+first_subfolder_path = os.path.join(extract_path, subfolders[0])
+labelmap_file = [file for file in os.listdir(folder_path) if file.endswith('.pbtxt')]
+labelmap_path = os.path.join(first_subfolder_path, labelmap_file[0])
+labelmap_txt_path = os.path.join(output_path, 'labelmap.txt')
+labelmap_pbtxt_path = os.path.join(output_path, 'labelmap.pbtxt')
 
-    with open(labelmap_txt_path, 'w') as f:
-        for item_name in parsed_data:
-            f.write(f'{item_name}\n')
+os.rename(labelmap_path, labelmap_pbtxt_path)
 
-    # Step 9: Delete the extracted folder and its contents
-    shutil.rmtree(extract_path)
+# Step 8: Take labelmap.pbtxt to create labelmap.txt tf standard
+with open(labelmap_pbtxt_path, 'r') as file:
+    labelmap_content = file.read()
 
+parsed_data = parse_labelmap(labelmap_content)
 
-if __name__ == "__main__":
-    main()
+with open(labelmap_txt_path, 'w') as f:
+    for item_name in parsed_data:
+        f.write(f'{item_name}\n')
+
+# Step 9: Delete the extracted folder and its contents
+shutil.rmtree(extract_path)
+
+# Time
+end_time_deletion = time.time()
+elapsed_time_deletion = end_time_deletion - end_time
+print(f"\n\n--Time : Deletion time: {elapsed_time_deletion:.4f} seconds--\n\n")
+complete_elapsed_time = end_time_deletion - start_time
+print(f"\n\n--Time : Complete Execution time: {complete_elapsed_time:.4f} seconds--\n\n")
